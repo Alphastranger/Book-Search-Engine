939cfa174b741c254c3bf9a78962a6f1039b6ed1
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-06-21 16:15:11 -0400

    Having problems with folders

---
+++ server/Schemas/resolvers.js
@@ -0,0 +1,40 @@
+const {Book, User} = require('../models')
+
+const resolvers = {
+    Query: {
+        books: async ()=> {
+            return Book.find({})
+        },
+        users: async (parent, {_id})=>{
+            const params = _id ? {_id} : {};
+            return User.find(params)
+        }
+    },
+    Mutation: {
+        createUser: async (parent, args) => {
+            const user = await User.create(args);
+            return user
+        },
+        saveBook: async (parent, {_id})=>{
+            const saveB = await Book.findOneandUpdate(
+                {_id: user_id},
+                {$addToSet: {savedBooks: body}},
+                {new: true}
+            )
+            return saveB
+        },
+        deleteUser: async(parent, {_id})=>{
+            const delUser = await User.findOneAndDelete(
+                {_id: user._id},
+                {$pull: {savedBooks: {bookId: params.bookId}}},
+                {new: true}
+            )
+            if (!delUser) {
+                return ({message: "Nobody here but us chickens"})
+            }
+            return delUser
+        }
+
+    }
+}
+module.exports = resolvers
