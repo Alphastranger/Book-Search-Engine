1f1001cac1c3e97073a405fecc3d15ee38615430
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-06-22 20:41:12 -0400

    Just added the word string in line 10 of typeDefs

--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -7,7 +7,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(author:[], description: String, title: String, bookId: Int, image: String, link: String): User
+        saveBook(author:[String], description: String, title: String, bookId: Int, image: String, link: String): User
         removeBook(bookId:Int): User
     }
     type Book {
